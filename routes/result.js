9b0266942d95f6e1c02ccc6aea9e0ba546089e4c
MarcusBuzette <marcusbuzete@me.com>
2018-02-12 14:40:14 -0800

    working on results

--- routes/result.js
+++ routes/result.js
@@ -1,6 +1,17 @@
 var data = require('../data.json');
 
+
 exports.view = function(req, res){
-	console.log(data);
-	res.render('result' , data);
+	var resultList = new Array();
+	
+	for (var i = data.places.length - 1; i >= 0; i--) {
+		if(data.places[i].mood == req.query.mood || data.places[i].task == req.query.task ){
+			console.log(data.places[i].name);
+			resultList.push(data.places[i]) ;
+		}
+		
+	}
+	console.log(resultList);
+
+	res.render('result' , {output: resultList});
 };
