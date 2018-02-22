51edac6e48952e1531a36b8c908db53405ae50a7
Lixiang Zhou <liz106@ucsd.edu>
2018-02-22 15:07:36 -0800

    addReview

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ var map = require("./routes/map");
 var navigation = require("./routes/navigation");
 var share = require("./routes/share");
 var settings = require("./routes/settings");
+var review = require('./routes/review');
 
 // Example route
 // var user = require('./routes/user');
@@ -68,6 +69,7 @@ app.get('/navigation',navigation.view);
 app.get('/navigationNL',navigation.viewNL);
 app.get('/share',share.view);
 app.get('/settings',settings.view);
+app.get('/review', review.addReview);
 
 // Example route
 // app.get('/users', user.list);
