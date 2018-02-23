6407fb781ba8bd4dbaa52669afdedb98da8319a4
MarcusBuzette <marcusbuzete@me.com>
2018-02-22 21:56:44 -0800

    loggednotlogged versions

--- routes/search.js
+++ routes/search.js
@@ -2,10 +2,12 @@ var data = require('../data.json');
 
 exports.view = function(req, res){
 	console.log(data);
+	data["logged"] = true;
 	res.render('search' , data);
 };
 
 exports.viewNL = function(req, res){
 	console.log(data);
-	res.render('searchNL' , data);
+	data["logged"] = false;
+	res.render('search' , data);
 };
