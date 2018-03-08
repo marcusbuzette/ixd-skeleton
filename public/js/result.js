b8605c665af9c82d6d14138de99d22b54f9d1806
MarcusBuzette <marcusbuzete@me.com>
2018-03-08 13:11:24 -0800

    trying

--- public/js/result.js
+++ public/js/result.js
@@ -4,6 +4,7 @@ $(function(){
 		ga('create', 'UA-114618844-1','auto');
 		ga('send', 'event', 'NoB', 'click');
 		$('.resultBtn').attr('href','/result_next_not_logged_in')
+		console.log("teste");
 		window.location.href = $(this).attr('href');
 	})
 })
